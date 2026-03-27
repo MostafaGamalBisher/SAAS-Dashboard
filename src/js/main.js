a0ca180261165103ee59a044fce81f2c5effd1f7
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-03-27 22:40:05 +0300

    add toggle nav bar javascript

--- src/js/main.js
+++ src/js/main.js
@@ -21,8 +21,18 @@ navBarElements.forEach((item) => {
 const asideToggle = document.querySelector('.aside__toggle');
 const aside = document.querySelector('.aside');
 const logo = document.querySelector('.logo');
+const navLabel = document.querySelectorAll('.nav-bar__label');
+const navBadge = document.querySelector('.nav-bar__badge');
+const illustration = document.querySelector('.illustration');
+const avatar = document.querySelector('.aside__bottom');
+const avatarTitle = document.querySelector('.avatar__title');
 
 asideToggle.addEventListener('click', () => {
   aside.classList.toggle('collapsed');
   logo.classList.toggle('logo-modifier');
+  navLabel.forEach((el) => el.classList.toggle('label-modifier'));
+  navBadge.classList.toggle('label-modifier');
+  illustration.classList.toggle('label-modifier');
+  avatar.classList.toggle('aside-bottom__modifier');
+  avatarTitle.classList.toggle('label-modifier');
 });
